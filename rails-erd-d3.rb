eae6ab04a5df29f2bdece4a68adf1aeae5c2917f
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-10 23:44:32 +0300

    add a new piece of code

--- rails-erd-d3.rb
+++ rails-erd-d3.rb
@@ -5,8 +5,20 @@ tables.each_with_index do |table, index|
   puts klass.column_names
 end
 
-klass.reflections.keys.each do |key|
+# Add all Models
+classes = []
+ObjectSpace.each_object(Class) do |c|
+  classes << c if c.superclass == ActiveRecord::Base
+end
+
+classes[].reflections.keys.each do |key|
   Post.reflections["comments"].table_name # => "comments"
   Post.reflections["comments"].macro # => :has_many
   Post.reflections["comments"].foreign_key # => "message_id"
 end
+
+classes[0]
+classes[0].reflections
+# cards
+classes[0].reflections["cards"].macro
+# :has_many
