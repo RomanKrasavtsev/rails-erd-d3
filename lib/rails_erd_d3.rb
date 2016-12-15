973aae36b6985d27478910126be781e30bd54521
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-12-15 23:34:02 +0300

    version 0.4.1

--- lib/rails_erd_d3.rb
+++ lib/rails_erd_d3.rb
@@ -27,6 +27,7 @@ class RailsErdD3
     Rails.application.eager_load!
     klass.connection
     @@models = ObjectSpace.each_object(Class).select { |o| o.ancestors.include? klass } || []
+    @@models = @@models - [klass] if @@models.include? klass
   end
 
   def self.get_data
